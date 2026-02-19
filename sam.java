8b7920ab5abb7a569c37e521b7266da22d2593f9
Suryaprakash Vk <suryasuryaprakash2005@gmail.com>
2026-02-19 20:16:55 +0530

    "fix : add sentense"

--- sam.java
+++ sam.java
@@ -3,6 +3,8 @@ public class sam
     public static void main(String[] args) {
 
         System.out.println("HI..............");
+                System.out.println("Hello.............");
+
         
     }
 }
